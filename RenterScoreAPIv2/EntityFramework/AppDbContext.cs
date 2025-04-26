4a9d0f8dcc2424c1bfa385d167dc4ad95bf9e643
Steven <e0390018@u.nus.edu>
2025-04-26 16:47:28 +0800

    property_role from users table

--- RenterScoreAPIv2/EntityFramework/AppDbContext.cs
+++ RenterScoreAPIv2/EntityFramework/AppDbContext.cs
@@ -3,9 +3,11 @@ namespace RenterScoreAPIv2.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using RenterScoreAPIv2.Property;
 using RenterScoreAPIv2.UserProfile;
+using RenterScoreAPIv2.User;
 
 public class AppDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<Property> Properties { get; set; }
     public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<User> Users { get; set; }
 }
