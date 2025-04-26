611d85f7c6b24e9af42edfe8cdd466c38fef5090
Steven <e0390018@u.nus.edu>
2025-04-27 01:11:28 +0800

    add end-to-end test in CI/CD pipeline

--- RenterScoreAPIv2.Test.EndToEnd/test/health.test.js
+++ RenterScoreAPIv2.Test.EndToEnd/test/health.test.js
@@ -4,6 +4,6 @@ import { expect } from 'chai';
 describe('health API testings', () => {
   it('check health', async () => {
     const response = await axios.get('http://localhost:5000/api/v2/health');
-    expect(response.status).to.equal(200);
+    expect(response.status).to.equal(201);
   });
 });
