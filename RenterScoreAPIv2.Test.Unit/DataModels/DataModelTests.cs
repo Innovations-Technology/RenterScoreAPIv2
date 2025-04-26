128aca3f347cc7b81dac4f3092f0e1678b04be23
Steven <e0390018@u.nus.edu>
2025-04-26 22:55:11 +0800

    add unit test to pass coverage scan

---
+++ RenterScoreAPIv2.Test.Unit/DataModels/DataModelTests.cs
@@ -0,0 +1,38 @@
+namespace RenterScoreAPIv2.Test.Unit.DataModels;
+
+using RenterScoreAPIv2.Property;
+using RenterScoreAPIv2.User;
+using RenterScoreAPIv2.UserProfile;
+using RenterScoreAPIv2.PropertyDetails;
+
+[TestFixture]
+public class DataModelTests
+{
+    [TestCase(typeof(Property))]
+    [TestCase(typeof(User))]
+    [TestCase(typeof(UserProfile))]
+    [TestCase(typeof(UserProfileViewModel))]
+    [TestCase(typeof(AddressViewModel))]
+    [TestCase(typeof(PropertyDetails))]
+    [TestCase(typeof(PropertyDetailsViewModel))]
+    public void TestGettersAndSetters(Type type)
+    {
+        var instance = Activator.CreateInstance(type);
+        foreach (var property in type.GetProperties())
+        {
+            if (property.CanRead)
+            {
+                property.GetValue(instance);
+            }
+            if (property.CanWrite)
+            {
+                object? defaultValue = null;
+                if (property.PropertyType.IsValueType)
+                {
+                    defaultValue = Activator.CreateInstance(property.PropertyType);
+                }
+                property.SetValue(instance, defaultValue);
+            }
+        }
+    }
+}
