70e96ee8ebc50145250f697baa2af129a67abad8
Steven <e0390018@u.nus.edu>
2025-04-26 14:17:24 +0800

    develop properties API

--- RenterScoreAPIv2/EntityFramework/AppDbContext.cs
+++ RenterScoreAPIv2/EntityFramework/AppDbContext.cs
@@ -2,8 +2,10 @@ namespace RenterScoreAPIv2.EntityFramework;
 
 using Microsoft.EntityFrameworkCore;
 using RenterScoreAPIv2.Property;
+using RenterScoreAPIv2.UserProfile;
 
 public class AppDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<Property> Properties { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
 }
