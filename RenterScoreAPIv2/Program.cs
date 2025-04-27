d59028e8c71bd68ad4021f5e316853db33d3f73d
Steven <e0390018@u.nus.edu>
2025-04-28 02:40:51 +0800

    fix to pass sonarqube

--- RenterScoreAPIv2/Program.cs
+++ RenterScoreAPIv2/Program.cs
@@ -8,7 +8,6 @@ using RenterScoreAPIv2.PropertyDetailsWithImages;
 using RenterScoreAPIv2.PropertyImage;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
