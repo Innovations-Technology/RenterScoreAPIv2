352569602a9b5a06c6a8deeafe92058d9741c760
Steven <e0390018@u.nus.edu>
2025-04-27 12:20:34 +0800

    remove JsonPropertyName

--- RenterScoreAPIv2/Property/AddressViewModel.cs
+++ RenterScoreAPIv2/Property/AddressViewModel.cs
@@ -1,19 +1,10 @@
 namespace RenterScoreAPIv2.Property;
 
-using System.Text.Json.Serialization;
-
 public class AddressViewModel
 {
-    [JsonPropertyName("block_no")]
     public string? BlockNo { get; set; }
-
-    [JsonPropertyName("postal_code")]
     public string? PostalCode { get; set; }
-
     public string? Region { get; set; }
-
     public string? Street { get; set; }
-
-    [JsonPropertyName("unit_no")]
     public string? UnitNo { get; set; }
 }
