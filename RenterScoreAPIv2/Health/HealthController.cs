85f2950803181fb964d3bcb04b182684c79edc8c
Steven <e0390018@u.nus.edu>
2025-05-10 01:04:09 +0800

    demo commit

--- RenterScoreAPIv2/Health/HealthController.cs
+++ RenterScoreAPIv2/Health/HealthController.cs
@@ -9,6 +9,6 @@ public class HealthController() : ControllerBase
     [HttpGet]
     public IActionResult CheckHealth()
     {
-        return Ok("Demo Test again");
+        return Ok("Demo Test 3");
     }
 }
